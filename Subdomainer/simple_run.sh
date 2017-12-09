344331506c3a61a88ced04569f269cc72cb18c89
EZG <eranzim@gmail.com>
2017-12-10 00:48:05 +0200

    Added a simple 1-click no-questions-asked script for Subdomainer

---
+++ Subdomainer/simple_run.sh
@@ -0,0 +1,9 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./simple_run.sh <domain>"
+    exit 1
+fi
+if [ ! -f subdomains_nc.txt ]; then
+    wget https://raw.githubusercontent.com/averagesecurityguy/axfr/master/subdomains_nc.txt
+fi
+echo -e "\n\nPress Ctrl-C to stop script..."
+python ./subdomainer.py -i subdomains_nc.txt -d $1 -o subdomains

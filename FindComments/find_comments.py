e1e627e88e7846e596b7591d58de55e520cacb20
EZG <eranzim@gmail.com>
2017-12-24 22:44:05 +0200

    Various small style fixes

--- FindComments/find_comments.py
+++ FindComments/find_comments.py
@@ -5,26 +5,29 @@ import bs4
 
 DIR_INDEX = 1
 SHOW_ERRORS = True
+BS_PARSER = 'lxml'
+
 
 def main():
     all_comments = []
     
     for directory, subdirectories, files in os.walk(sys.argv[DIR_INDEX]):
-        for file in files:
-            file_path = os.path.join(directory, file)
+        for curr_file in files:
+            file_path = os.path.join(directory, curr_file)
             try:
                 with open(file_path, "rb") as f:
-                    soup = bs4.BeautifulSoup(f.read(), 'lxml')
-                all_comments += soup.findAll(text=lambda text:isinstance(text, bs4.Comment))
-            #except bs4.HTMLParser.HTMLParseError:
+                    soup = bs4.BeautifulSoup(f.read(), BS_PARSER)
+                all_comments += soup.findAll(text=lambda text: isinstance(text, bs4.Comment))
             except IOError:
                 if SHOW_ERRORS:
                     sys.stderr.write("Failed parsing file \"{}\"\n".format(file_path))
     
     all_comments = list(set(all_comments))
     all_comments.sort()
+    print "Comments:"
     for comment in all_comments:
         print comment
+
 
 if __name__ == "__main__":
     assert len(sys.argv) == 2, "USAGE: find_comments.py <containing_dir>"

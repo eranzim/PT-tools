ba4af7c356a0606460ef5fa7cb55fa45b3e52bd2
EZG <eranzim@gmail.com>
2017-12-24 23:06:16 +0200

    Added option to scan a single file, and overall improved the script.

--- FindComments/find_comments.py
+++ FindComments/find_comments.py
@@ -3,32 +3,53 @@ import os
 import sys
 import bs4
 
-DIR_INDEX = 1
+PATH_INDEX = 1
 SHOW_ERRORS = True
 BS_PARSER = 'lxml'
+
+
+def extract_from_file(file_path):
+    try:
+        with open(file_path, "rb") as f:
+            soup = bs4.BeautifulSoup(f.read(), BS_PARSER)
+        # Return a list of all the comments in the file
+        return soup.findAll(text=lambda text: isinstance(text, bs4.Comment))
+    except IOError:
+        if SHOW_ERRORS:
+            sys.stderr.write("Failed parsing file \"{}\"\n".format(file_path))
+        return []
+
+
+def print_comments(all_comments):
+    # If we haven't found anything:
+    if not all_comments:
+        print "\nNo comments found... :("
+        return
+    
+    # Strip, remove duplicates and sort
+    all_comments = [comment.strip() for comment in all_comments]
+    all_comments = list(set(all_comments))
+    all_comments.sort()
+    print "\nComments:"
+    print "---------\n"
+    for comment in all_comments:
+        print comment
 
 
 def main():
     all_comments = []
     
-    for directory, subdirectories, files in os.walk(sys.argv[DIR_INDEX]):
-        for curr_file in files:
-            file_path = os.path.join(directory, curr_file)
-            try:
-                with open(file_path, "rb") as f:
-                    soup = bs4.BeautifulSoup(f.read(), BS_PARSER)
-                all_comments += soup.findAll(text=lambda text: isinstance(text, bs4.Comment))
-            except IOError:
-                if SHOW_ERRORS:
-                    sys.stderr.write("Failed parsing file \"{}\"\n".format(file_path))
-    
-    all_comments = list(set(all_comments))
-    all_comments.sort()
-    print "Comments:"
-    for comment in all_comments:
-        print comment
+    if os.path.isdir(sys.argv[PATH_INDEX]):
+        for directory, subdirectories, files in os.walk(sys.argv[PATH_INDEX]):
+            for curr_file in files:
+                file_path = os.path.join(directory, curr_file)
+                all_comments += extract_from_file(file_path)
+    else:
+        all_comments = extract_from_file(sys.argv[PATH_INDEX])
+
+    print_comments(all_comments)
 
 
 if __name__ == "__main__":
-    assert len(sys.argv) == 2, "USAGE: find_comments.py <containing_dir>"
+    assert len(sys.argv) == 2, "USAGE: find_comments.py <dir_or_file_path>"
     main()

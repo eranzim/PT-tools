b46c84723979bc57224e62133c71e59f315fa2c9
EZG <eranzim@gmail.com>
2017-12-24 23:38:02 +0200

    Fixed a few path-related bugs

--- SubdomainDataExtractor/simple_run.sh
+++ SubdomainDataExtractor/simple_run.sh
@@ -3,9 +3,10 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: ./simple_run.sh <domain>"
     exit 1
 fi
+chmod +x ./extractor.sh
 if [ ! -f ../Subdomainer/subdomains_nc.txt ]; then
-    wget https://raw.githubusercontent.com/averagesecurityguy/axfr/master/subdomains_nc.txt
+    wget -O ../Subdomainer/subdomains_nc.txt https://raw.githubusercontent.com/averagesecurityguy/axfr/master/subdomains_nc.txt
 fi
 echo -e "\n\nPress Ctrl-C to stop script..."
-python ../Subdomainer/subdomainer.py -i subdomains_nc.txt -d $1 -n 1000 -o subdomains
-./extractor.sh subdomains $1
+python ../Subdomainer/subdomainer.py -i ../Subdomainer/subdomains_nc.txt -d $1 -n 1000 -o ../Subdomainer/subdomains
+./extractor.sh ../Subdomainer/subdomains $1

9f9c7ffc2657cd13ef368964a60769b5f003b8ef
EZG <eranzim@gmail.com>
2018-01-07 12:02:34 +0200

    Get vars immediately following semicolons

--- PHPObfuscator/obfuscate.py
+++ PHPObfuscator/obfuscate.py
@@ -1,27 +1,30 @@
 import sys
 import re
 
+# TODO: change var names
+
 PHP_EXT = ".php"
-VARIABLE_REGEX = re.compile(r'(?:^\$(?P<variable>\w+))|(?:\s+\$(?P<variable2>\w+))')
-PREDEFINED_VARS = set("GLOBALS", "_SERVER", "_GET", "_POST", "_FILES", "_REQUEST", "_SESSION", "_ENV",
-                      "_COOKIE", "php_errormsg", "HTTP_RAW_POST_DATA", "http_response_header", "argc", "argv")
+VARIABLE_REGEX = re.compile(r'(?:^\$(?P<variable1>\w+))|(?:\s+\$(?P<variable2>\w+))|(?:;\$(?P<variable3>\w+))')
+PREDEFINED_VARS = {"GLOBALS", "_SERVER", "_GET", "_POST", "_FILES", "_REQUEST", "_SESSION", "_ENV", "_COOKIE",
+                   "php_errormsg", "HTTP_RAW_POST_DATA", "http_response_header", "argc", "argv"}
 
 
 def get_all_variables(php):
     regex_result = VARIABLE_REGEX.findall(php)
     all_vars = set()
     for result in regex_result:
-        if result["variable"] is not None:
-            all_vars.add(result["variable"])
-        elif result["variable2"] is not None:
-            all_vars.add(result["variable2"])
-        else:
-            raise Exception("Unexpected error in regex parsing")
+        for regex_variable_group in result:
+            if regex_variable_group:
+                all_vars.add(regex_variable_group)
+                break
+
     return all_vars.difference(PREDEFINED_VARS)
 
 
 def obfuscate(php):
-    print get_all_variables(php)
+    vars = get_all_variables(php)
+    print vars
+    print len(vars)
     return php
 
 
